82d0ba86c755a84df406b7abac5759fee78e41f1
Albert Lee <Albert@Alberts-MacBook-Pro.local>
2017-03-24 19:21:50 -0700

    about / services

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/about', to: 'pages#about'
   get ':slug', to: 'pages#show'
 
   root to: 'pages#index'
