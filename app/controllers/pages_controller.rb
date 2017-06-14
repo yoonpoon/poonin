eb80b70c45ca131b5dfca8e83d00537102433517
Albert Lee <Albert@Alberts-MacBook-Pro.local>
2017-06-14 13:09:15 -0700

    pretty url / :root

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
-
   def index
     @nav = Nav.all.load.first
     @pages = @nav.pages
   end
 
   def show  
-    @page = Page.find(params[:id])
+    @page = Page.find_by(slug: params[:slug]).load.first
+    return redirect_to :root unless @page
   end
 
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,4 @@
 class Page < ContentfulModel::Base
+  return_nil_for_empty :content, :excerpt  
   self.content_type_id = 'page'
-  return_nil_for_empty :content, :excerpt
 end 
