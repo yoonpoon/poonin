35fe35b2d24d8f4a292eac4e7fea408f152da9b2
Albert Lee <Albert@Alberts-MacBook-Pro.local>
2017-06-21 18:02:39 -0700

    kerning.js / added new projects / higher-res images

--- app/assets/javascripts/script.js
+++
@@ -1,7 +0,0 @@
-// var $grid = $('.grid').imagesLoaded( function() {
-//   // init Packery after all images have loaded
-//   $grid.packery({
-//     itemSelector: '.grid-item',
-//     percentPosition: true
-//   });
-// });  
