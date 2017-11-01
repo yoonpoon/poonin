cd51c5974472d46b4ded850201306bc5393c04ec
Albert Lee <yoonpoon@gmail.com>
2017-10-31 20:56:35 -0700

    return nil for empty fields

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,5 +4,7 @@ class Page < ContentfulModel::Base
   return_nil_for_empty  :content,
                         :excerpt,
                         :team,
-                        :projTitle
+                        :projTitle,
+                        :role,
+                        :projectTitle
 end
